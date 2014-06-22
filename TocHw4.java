4a5345911eee4b3b7cc6088c346762de1c9abd59
Aquile Sanchez <akyelshz@users.noreply.github.com>
2014-06-23 04:01:49 +0800

    Add Comments.

--- TocHw4.java
+++ TocHw4.java
@@ -1,3 +1,15 @@
+/*
+ * THEORY OF COMPUTATION - HW4
+ * 資工系104乙班
+ * 沙江益 - Aquile R. Sanchez
+ * F74007094
+ * File Name: TocHw4.java
+ * Run Syntax: java -jar TocHw4.jar URL
+ * Output: Given a URL which can load a Json file containing less than 2000 pieces of house trading
+ * data in Taiwan, it scans the whole data and find out “which road in a city has house trading
+ * records spread in #max_distinct_month.
+*/
+
 import java.net.*;
 import java.util.*;
 import java.util.regex.Matcher;
